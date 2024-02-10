cc45449e51bf28a3206931887911f7188f8bc442
yahya-ahdb <eheyaahdab@gmail.com>
2024-02-10 10:51:53 +0300

    add

--- js/main.js
+++ js/main.js
@@ -178,4 +178,24 @@ function scrollToTop() {
       });
 }
 
+var swiper = new Swiper(".mySwiper", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+//   autoplay: {
+//     delay: 3000, 
+//     disableOnInteraction: false,
+// },
 
+});
